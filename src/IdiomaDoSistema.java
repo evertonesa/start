75e36efb1731535a2f458bbc4d31ba282e241f9f
Everton Santos <everton.santos@db.tec.br>
2023-08-28 23:20:22 -0300

    Comitt Dia 01

---
+++ src/IdiomaDoSistema.java
@@ -0,0 +1,9 @@
+import java.util.Locale;
+
+public class IdiomaDoSistema {
+    public static void main(String[] args) {
+        Locale linguagem = new Locale("Portugues", "Brasil");
+        System.out.println("O Idioma do sistema e: ");
+        System.out.println(linguagem);
+    }
+}
